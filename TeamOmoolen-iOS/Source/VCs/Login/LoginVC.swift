d02fa1d1fc76f718b159a7a6d571d1a0b55eff63
hyungyu Kim <ypsophila97@icloud.com>
2021-06-30 20:02:45 +0900

    [#3] SecondOnboarding 레이아웃 구현
    ### Description
    - SecondOnboarding 레이아웃 구현
    - model 설정
    
    ### Related Issue
    close #3

--- TeamOmoolen-iOS/Source/VCs/Login/LoginVC.swift
+++ TeamOmoolen-iOS/Source/VCs/Login/LoginVC.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    // MARK: -
+    
+    // MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+    // MARK: -
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+// MARK: -
