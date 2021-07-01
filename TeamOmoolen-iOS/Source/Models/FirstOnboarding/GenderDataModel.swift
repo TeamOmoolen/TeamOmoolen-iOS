da27652aa8642ce7710b18a8196e0fc79df57b65
pcsoyeon <alice9809@naver.com>
2021-07-01 15:39:27 +0900

    [Feat] 온보딩1 레이아웃 구현

---
+++ TeamOmoolen-iOS/Source/Models/FirstOnboarding/GenderDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  GenderModel.swift
+//  TeamOmoolen-iOS
+//
+//  Created by soyeon on 2021/06/30.
+//
+
+import Foundation
+
+struct GenderDataModel {
+    var genderImage: String
+    var gender: String
+}
