ea39ec81b0d2a633a6b4ac6f69d84cfe7123e4bf
so yeon <alicesysy@swu.ac.kr>
2021-07-03 23:14:18 +0900

    [#31] Home 뷰 초기 세팅

---
+++ TeamOmoolen-iOS/Source/VCs/Home/HomeVC.swift
@@ -0,0 +1,67 @@
+//
+//  HomeVC.swift
+//  TeamOmoolen-iOS
+//
+//  Created by soyeon on 2021/07/03.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    // MARK: - UI Components
+    
+
+    // MARK: - Local Variables
+    
+    
+    // MARK: - View Life Cycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setList()
+        
+        registerXib()
+        setHomeTableView()
+    }
+}
+
+// MARK:- Custom Methods
+
+extension HomeVC {
+    func setUI() {
+        
+    }
+    
+    func setList() {
+        
+    }
+    
+    func registerXib() {
+        
+    }
+    
+    func setHomeTableView() {
+        
+    }
+}
+
+// MARK: - UITableView Delegate
+
+extension HomeVC: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableView DataSource
+
+extension HomeVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
