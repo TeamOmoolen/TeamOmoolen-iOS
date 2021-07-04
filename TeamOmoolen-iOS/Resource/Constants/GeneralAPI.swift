19611636051e98e25e72b378738aaf8bbb7468d8
hyungyu Kim <ypsophila97@icloud.com>
2021-07-04 23:59:22 +0900

    [#37] Moya manager 붙이기
    ### Descriptions
    - Moya api, service 붙이기
    - Network 폴더링
    ### Related Issue
    close #37

---
+++ TeamOmoolen-iOS/Resource/Constants/GeneralAPI.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  TeamOmoolen-iOS
+//
+//  Created by kimhyungyu on 2021/07/04.
+//
+
+import Foundation
+
+extension Const {
+    struct GeneralAPI {
+        //        static let baseURL = "https://api.themoviedb.org/3"
+    }
+}
