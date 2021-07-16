8bbb4d86162578c9463532c36ff10955123c6c55
hyungyu Kim <ypsophila97@icloud.com>
2021-07-16 22:35:52 +0900

    [#216] LoadingLogoVC 생성
    ### Descriptions
    - LoadingLogoVC 생성
    - 1초 동안 present
    
    ### Related Issue
    close #216

--- TeamOmoolen-iOS/Source/VCs/Home/LoadingLogoVC.swift
+++ TeamOmoolen-iOS/Source/VCs/Home/LoadingLogoVC.swift
@@ -11,9 +11,9 @@ class LoadingLogoVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
-            self.dismiss(animated: true, completion: nil)
-        }
+//        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+//            self.dismiss(animated: true, completion: nil)
+//        }
         // Do any additional setup after loading the view.
     }
 }
--- TeamOmoolen-iOS/Source/VCs/Search/FilterVC.swift
+++ TeamOmoolen-iOS/Source/VCs/Search/FilterVC.swift
@@ -416,7 +416,7 @@ extension FilterVC {
             }
             searchResultVC.modalPresentationStyle = .fullScreen
             searchResultVC.modalTransitionStyle = .crossDissolve
-            searchResultVC.totalCount = (self.searchResultResponse?.products.count)!
+            searchResultVC.totalCount = self.searchResultResponse?.totalCount ?? 0
             searchResultVC.resultList = self.searchResultResponse?.products
             self.navigationController?.pushViewController(searchResultVC, animated: true)
         }
